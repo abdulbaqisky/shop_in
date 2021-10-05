6ea2b022ca1b8c96eb6845a0c5beb3685ad1d1a5
Adekomi AbdulBaaqi <adekomiabdulbaqi3@gmail.com>
2021-10-05 18:24:46 +0100

    dismiss cart card

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -51,8 +51,10 @@ class CartScreen extends StatelessWidget {
               child: ListView.builder(
             itemBuilder: (ctx, i) => CartItem(
               cart.items.values.toList()[i].id,
+              cart.items.keys.toList()[i],
               cart.items.values.toList()[i].price,
               cart.items.values.toList()[i].quantity,
+              cart.items.values.toList()[i].title,
             ),
             itemCount: cart.items.length,
           ))
